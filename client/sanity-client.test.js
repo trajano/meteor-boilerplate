2eafebb204113aa47a33f9e58563fcb57e516dcb
Archimedes Trajano <arch@trajano.net>
2016-04-30 02:11:34 -0400

    Used ES6 modules and created API module stub

---
+++ client/sanity-client.test.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+/**
+ * @test
+ */
+describe('sanity test only on client', function () {
+  it('can test something', function () {})
+})
