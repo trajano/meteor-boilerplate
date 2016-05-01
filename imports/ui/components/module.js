069c71cf734356faa25dac7778c33ec5a6fa29b8
Archimedes Trajano <arch@trajano.net>
2016-05-01 01:26:16 -0400

    Improved JSDoc tagging
    This sets up memberof tags to represent the documentation as a tree
    better.
    
    JSDoc should be present on the top of the JS file.

--- imports/ui/components/module.js
+++ imports/ui/components/module.js
@@ -1,1 +1,14 @@
+/**
+ * UI Components Angular module definition.
+ *
+ * This uses the name `module.js` rather than `ui.components.module.js` as
+ * each AngularJS module would have a `module.js` and index.js it would
+ * allow for easier module renames.
+ *
+ * The `namespace` tag should be quoted as it has dots, sorts it to the top
+ * and gives it a visual difference.
+ *
+ * @namespace "ui.components"
+ * @memberof module:/imports/ui/components
+ */
 export default angular.module('ui.components', [])
