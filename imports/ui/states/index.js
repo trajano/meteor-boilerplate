913b0addf3e3290e86e1bcfbaa4f535d41bf5e3c
Archimedes Trajano <arch@trajano.net>
2016-04-30 23:50:07 -0400

    Sign-in state
    This adds the notion of an abstract state representing authenticated and
    unauthenticated.  In addition, the atForm is removed from home and put
    into the sign-in state.  The currentUser is shown on the home state.
    
    The onSubmitHook is implemented to better integrate the AccountsTemplates
    workflow with the boilerplate.

--- imports/ui/states/index.js
+++ imports/ui/states/index.js
@@ -10,9 +10,21 @@
  * each state to be isolated and remove the need for a global route
  * configuration.
  *
+ * There are also two abstract parent states defined: `unauthenticated` and
+ * `authenticated` that need to be applied to the top-level states as their
+ * `parent`.
+ *
  * @module /imports/ui/states
  */
 import module from './module.js'
+
+// abstract states
+import './authenticated'
+import './unauthenticated'
+
+// top-level states
 import './home'
 import './not-found'
+import './sign-in'
+
 export default module.name
