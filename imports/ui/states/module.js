82adfc5e23044e5f2e3ba5f4f749461804172510
Archimedes Trajano <arch@trajano.net>
2016-04-30 23:50:07 -0400

    Sign-in state
    This adds the notion of an abstract state representing authenticated and
    unauthenticated.  In addition, the atForm is removed from home and put
    into the sign-in state.  The currentUser is shown on the home state.
    
    The onSubmitHook is implemented to better integrate the AccountsTemplates
    workflow with the boilerplate.

--- imports/ui/states/module.js
+++ imports/ui/states/module.js
@@ -7,11 +7,45 @@ import ngUiRouter from 'angular-ui-router'
 import uiComponents from '/imports/ui/components'
 import { Meteor } from 'meteor/meteor'
 
+/**
+ * Authentication required.
+ * @type {string}
+ */
+export const AUTH_REQUIRED = 'AUTH_REQUIRED'
+
+/**
+ * Authenticated already.
+ * @type {string}
+ */
+export const ALREADY_AUTHENTICATED = 'ALREADY_AUTHENTICATED'
+
 export default angular.module('ui.states', [ngUiRouter, uiComponents])
-  .run(($rootScope, $log, $urlMatcherFactory) => {
-    if (!Meteor.isProduction) {
-      $rootScope.$on('$destroy', $rootScope.$on('$stateChangeError', $log.log.bind($log)))
-    }
+  .run(($rootScope, $log, $urlMatcherFactory, $state) => {
+    $rootScope.$on('$destroy', $rootScope.$on('$stateChangeError',
+      /**
+       * Handle the state change error.  If the error is `AUTH_REQUIRED`,
+       * set the state to `sign-in`.  If the error is `ALREADY_AUTHENTICATED`,
+       * set the state to `home`.  All other conditions log the state change
+       * error details when not running in production.
+       *
+       * @param {event} event
+       * @param {string} toState
+       * @param {*} toParams
+       * @param {string} fromState
+       * @param {*} fromParams
+       * @param {string} error
+       */
+      (event, toState, toParams, fromState, fromParams, error) => {
+        if (error === AUTH_REQUIRED) {
+          $state.go('sign-in')
+        } else if (error === ALREADY_AUTHENTICATED) {
+          $state.go('home')
+        } else {
+          if (!Meteor.isProduction) {
+            $log.log(event, toState.name, toParams, fromState.name, fromParams, error)
+          }
+        }
+      }))
     $urlMatcherFactory.strictMode(false)
   })
   .config(($urlRouterProvider) => {
