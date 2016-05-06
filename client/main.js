727f445cdb1d27cdeb9597ad871a5057779e2a23
Archimedes Trajano <Archimedes.Trajano@ontario.ca>
2016-05-06 14:57:59 -0400

    Bootstrap from main.js rather than index.html
    This will help prepare for Angular 2 migration.

--- client/main.js
+++ client/main.js
@@ -1,2 +1,7 @@
-import 'angular'
+import angular from 'angular'
 import '/imports/ui'
+
+// eslint-disable-next-line angular/document-service
+angular.bootstrap(document.body, ['ui.app'], {
+  strictDi: true
+})
